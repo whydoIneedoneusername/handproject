a9254d8c36532aeff210ca1d6f586af28743f5dd
whydoIneedoneusername <100236031+whydoIneedoneusername@users.noreply.github.com>
2023-03-09 15:39:55 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+https://teachablemachine.withgoogle.com/models/DVAqMhtW2/model.JSON
+
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/DVAqMhtW2/model.json', modelReady);
+    
+}
+
+
+
+Webcam.set({
+  width:350,
+  height:300,
+  image_format:'png',
+  png_quality:90,
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+  Webcam.snap(function(data_uri))
+
+  {
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    
+  }
+}
